ee512d4b7a19e6e0bf5ab65508e9b615f46e3866
Dig2 <55265626+Dig2@users.noreply.github.com>
2022-04-24 21:12:09 +0800

    add support for bsc, avax

--- config.py
+++ config.py
@@ -1,6 +1,32 @@
-ETHERSCAN_API_KEY = "FILL IT!"
-ETHERSCAN_MAINNET_GET_SOURCE_CODE_URL = f"https://api.etherscan.io/api?module=contract&action=getsourcecode&address={{}}&apikey={ETHERSCAN_API_KEY}"
-ETHEREUM_RPC_URL = "https://rpc.flashbots.net"
+class ChainId:
+    ETHEREUM = 1
+    BSC = 56
+    AVALANCHE = 43114
+
+API_KEY = {
+    ChainId.ETHEREUM: "",
+    ChainId.BSC: "",
+    ChainId.AVALANCHE: ""
+}
+
+BROWSER_URL = {
+    ChainId.ETHEREUM: "https://etherscan.io",
+    ChainId.BSC: "https://bscscan.com",
+    ChainId.AVALANCHE: "https://snowtrace.io"
+}
+
+GET_SOURCE_CODE_URL = {
+    ChainId.ETHEREUM: f"https://api.etherscan.io/api?module=contract&action=getsourcecode&address={{}}&apikey={API_KEY[ChainId.ETHEREUM]}",
+    ChainId.BSC: f"https://api.bscscan.com/api?module=contract&action=getsourcecode&address={{}}&apikey={API_KEY[ChainId.BSC]}",
+    ChainId.AVALANCHE: f"https://api.snowtrace.io/api?module=contract&action=getsourcecode&address={{}}&apikey={API_KEY[ChainId.AVALANCHE]}"
+}
+
+RPC_URL = {
+    ChainId.ETHEREUM: "https://rpc.flashbots.net",
+    ChainId.BSC: "https://bsc-dataseed.binance.org/",
+    ChainId.AVALANCHE: "https://api.avax.network/ext/bc/C/rpc"
+}
+
 
 class bcolors:
     HEADER = '\033[95m'

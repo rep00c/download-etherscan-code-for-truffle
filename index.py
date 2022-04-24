859846a4c0327aa710a1e64c987abc4eaaa8b6cf
Dig2 <55265626+Dig2@users.noreply.github.com>
2022-04-24 21:12:43 +0800

    add support for bsc, avax

--- index.py
+++ index.py
@@ -9,9 +9,18 @@ def import_replace(matched):
 
 def pre_check():
     global contract_address
+    global chain_id
+    
+    if "snowtrace" in sys.argv[1]:
+        chain_id = ChainId.AVALANCHE  # AVAX
+    elif "bscscan" in sys.argv[1]:
+        chain_id = ChainId.BSC  # BSC
+    else:
+        chain_id = ChainId.ETHEREUM  # Ethereum\
+    print("Chain id:", bcolors.OKGREEN, chain_id, bcolors.ENDC)
 
     if (len(sys.argv) != 2):
-        sys.exit(bcolors.FAIL + "Usage: soc <URL>" + bcolors.ENDC)
+        sys.exit(bcolors.FAIL + "Usage: need exactly 2 parameters, <URL>" + bcolors.ENDC)
 
     res = re.findall(r'.*([0-9a-zA-Z]{40}).*', sys.argv[1])
 
@@ -23,10 +32,11 @@ def pre_check():
 
 def real_res():
     global contract_address
+    global chain_id
 
     while True:
         print( "Searching solidity code of address:", bcolors.OKGREEN , contract_address, bcolors.ENDC)
-        res = requests.get(ETHERSCAN_MAINNET_GET_SOURCE_CODE_URL.format(contract_address)).json()
+        res = requests.get(GET_SOURCE_CODE_URL[chain_id].format(contract_address)).json()
         contract_name = res['result'][0]['ContractName']
 
         if contract_name in ['UpgradeableProxy', 'TransparentUpgradeableProxy', 'AdminUpgradeabilityProxy', 'BeaconProxy']:
@@ -34,11 +44,11 @@ def real_res():
             if c != 'n':
                 slot = "0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50" if contract_name in ['BeaconProxy'] else "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
                 payload = {"jsonrpc":"2.0", "method": "eth_getStorageAt", "params": [contract_address, slot, "latest"], "id": 1}
-                logic_address = "0x" + requests.post(ETHEREUM_RPC_URL, json = payload).json()['result'][-40:]
+                logic_address = "0x" + requests.post(RPC_URL[chain_id], json = payload).json()['result'][-40:]
                 contract_address = logic_address
                 print( "Found logic contract address:", bcolors.OKGREEN , contract_address, bcolors.ENDC)
                 if contract_name in ['BeaconProxy']:
-                    sys.exit(bcolors.FAIL + f"Beacon proxy contract pattern: https://etherscan.io/address/{contract_address}#code. Please check it manually." + bcolors.ENDC)
+                    sys.exit(bcolors.FAIL + f"Beacon proxy contract pattern: {BROWSER_URL[chain_id]}/address/{contract_address}#code. Please check it manually." + bcolors.ENDC)
                 continue
         break
 
@@ -72,12 +82,19 @@ def write_files(codes, contract_name, base_dir):
                 f.write(data) 
     else:
         each_files = re.findall(r"\/\/ File ([\s\S]*?\.sol)([\s\S]*?)(?=\/\/ File|$)", codes)
+
+        if len(each_files) == 0:
+            full_filename = os.path.join(base_dir, contract_name+".sol")
+            print("Writing file:", bcolors.OKBLUE, full_filename, bcolors.ENDC)
+            with open(full_filename, "w") as f:
+                f.write(codes)
+            return
+
         last_file = ""
         for (filename, code) in each_files:
             full_filename = os.path.join(base_dir, os.path.basename(filename))
             print("Writing file:", bcolors.OKBLUE, full_filename, bcolors.ENDC)
             with open(full_filename, "w") as f:
-                print(code.strip("\r\n"))
                 f.write((f"import \"./{last_file}\";\r\n" if last_file else "") + code.strip("\r\n"))
 
             last_file = os.path.basename(filename)
@@ -89,7 +106,6 @@ def work():
 
     contract_name = res['result'][0]['ContractName']
     codes = res['result'][0]['SourceCode']
-
     base_dir = os.path.join(os.getcwd(), "contracts", contract_name)
 
     create_directory(base_dir)  # create directory: ./contracts/{contract name}
